298def58a0e1e35599c2e6e45b6163c217b2a197
fredcommo <fredcommo@gmail.com>
2023-03-11 13:45:32 +0100

    refactor: split tasks as functions

--- src/main.py
+++ src/main.py
@@ -26,15 +26,17 @@ from optimizers import XGBClassifier_optimizer
 ############################################
 # Other utils:
 def clean_param_names(params):
+    """
+    Retrieve the original param names,
+    so they can be passed to the best model
+    """
     clean = lambda p: "_".join(p.split("_")[1:])
     return {clean(p): v for p, v in params.items()}
 
 
 ############################################
 # Define an objective function to be maximized:
-def objective(trial, X_train, y_train):
-
-    model_list = ["LogisticRegression", "RandomForestClassifier", "XGBClassifier"]
+def objective(trial, X_train, y_train, model_list):
 
     # Setup values for the hyperparameters optimization:
     classifier_name = trial.suggest_categorical("classifier", model_list)
@@ -48,7 +50,38 @@ def objective(trial, X_train, y_train):
     return score.mean()
 
 
-def run_best_model(X_train, X_test, y_train, y_test, best_classifier, best_params):
+def optimize(X_train, y_train, n_trials=5):
+    """
+    Run optimizer
+    note: to pass args to the objective func, wrap it inside a lambda func + args
+    and call the lambda func in study.optimize()
+    """
+    model_list = ["LogisticRegression", "RandomForestClassifier", "XGBClassifier"]
+    objective_func = lambda trial: objective(trial, X_train, y_train, model_list)
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective_func, n_trials=n_trials, show_progress_bar=True)
+
+    return study
+
+
+def get_best_model(study):
+    best_value = study.best_value
+    best_params = study.best_params
+    best_classifier = best_params.pop('classifier')
+    best_params = clean_param_names(best_params)
+
+    ############################################
+    # Getting the best result
+    print(f"\nBest accuracy: {best_value}")
+    print(f"Best algorithm: {best_classifier}")
+    print(f"Best parameters (ready to use): {best_params}\n")
+
+    return best_classifier, best_params
+
+
+def train_best_model(study, X_train, X_test, y_train, y_test):
+
+    best_classifier, best_params = get_best_model(study)
 
     print(f"Running {best_classifier} as best classifier")
     print("Params:")
@@ -64,36 +97,11 @@ def run_best_model(X_train, X_test, y_train, y_test, best_classifier, best_param
     return classifier
 
 
-def main(X_train, X_test, y_train, y_test, n_trials=5):
+def show_cross_tab(observed, predictions):
+    result = pd.crosstab(observed, predictions, rownames=["Observed"], colnames=["Predicted"])
+    print("\nResults on test set:")
+    print(result)
 
-    ############################################
-    # Running optimizer
-    # to add args to the objective: Wrap the objective inside a lambda and call objective inside it
-    objective_func = lambda trial: objective(trial, X_train, y_train)
-    study = optuna.create_study(direction="maximize")
-    study.optimize(objective_func, n_trials=n_trials, show_progress_bar=True)
-
-    best_value = study.best_value
-    best_params = study.best_params
-    best_classifier = best_params.pop('classifier')
-
-    # Retrieve the original param names,
-    # so they can be directly passed to the best algo
-    best_params = clean_param_names(best_params)
-
-    ############################################
-    # Getting the best result
-    print()
-    print(f"Best accuracy: {best_value}")
-    print(f"Best algorithm: {best_classifier}")
-    print(f"Best parameters (ready to use): {best_params}")
-    print()
-
-    ############################################
-    # Running the best model
-    classifier = run_best_model(X_train, X_test, y_train, y_test, best_classifier, best_params)
-
-    return classifier
 
 def plot(df):
     plt.figure(figsize=(12, 8))
@@ -108,27 +116,30 @@ def plot(df):
     plt.show()
 
 
-if __name__ == "__main__":
-
+def main():
     ############################################
     # Grab a sklearn Classification dataset:
     X, y = datasets.load_breast_cancer(return_X_y=True, as_frame=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=123)
 
     ############################################
-    # Run optimizer and get the best model:
-    classifier = main(X_train, X_test, y_train, y_test)
-    prediction = classifier.predict(X_test)
+    # Run the optimizer:
+    study = optimize(X_train, y_train)
+
+    ############################################
+    # Train the classifier and make predictions:
+    classifier = train_best_model(study, X_train, X_test, y_train, y_test)
+    predictions = classifier.predict(X_test)
 
     ############################################
     # Show results:
-    result = pd.crosstab(y_test, prediction, rownames=["Observed"], colnames=["Predicted"])
-    print("\nResults on test set:")
-    print(result)
+    show_cross_tab(y_test, predictions)
 
     pca = decomposition.PCA(n_components=3)
     pca.fit(X)
-
     new_x = pca.transform(X_test)
-    df = pd.DataFrame({"x1": new_x[:,0], "x2": new_x[:,1], "y": prediction})
+    df = pd.DataFrame({"x1": new_x[:,0], "x2": new_x[:,1], "y": predictions})
     plot(df)
+
+if __name__ == "__main__":
+    main()
